7f5b8a4ab8e38c4f1025d4409d1592df801decb1
Leesunwoo1468 <55747258+Leesunwoo1468@users.noreply.github.com>
2019-10-03 02:30:35 +0900

    Floating,Enum

---
+++ C# Basic/02/Floating_Point/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Floating_Point
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.1415_9265_3589_7932_3846_2643_3832_79f;
+            Console.WriteLine(a);
+
+            double b = 3.1415_9265_3589_7932_3846_2643_3832_79;
+            Console.WriteLine(b);
+
+            decimal c = 3.1415_9265_3589_7932_3846_2643_3832_79m;
+            Console.WriteLine(c);
+
+            //부동소수점
+        }
+    }
+}
